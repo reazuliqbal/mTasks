bce643b6ef4ebbd034d8cdd7cd06587a663ba2d4
CodeBull <admin@reazul.net>
2018-05-03 11:08:45 +0600

    Rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-console": 0,
-        "no-underscore-dangle": 0,
+        "no-underscore-dangle": ["error", { allow: ["_id"] }],
         "no-shadow": 0,
         "no-param-reassign": 0
     }
